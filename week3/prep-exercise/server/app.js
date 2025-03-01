d11f768abca2140814f4208ad862a48ad45b8dda
Hossein <Hossein.kelisa@gmail.com>
2025-03-01 21:55:13 +0100

    prep-exercise w3 done

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
-// TODO Use below import statement for importing middlewares from users.js for your routes
-// TODO import { ....... } from "./users.js";
+import { register, login, getProfile, logout } from './users.js';
 
 let app = express();
-
 app.use(express.json());
-// TODO: Create routes here, e.g. app.post("/register", .......)
+
+app.post('/auth/register', register);  //this will register a new user
+
+app.post('/auth/login', login);   //this will login a user
+
+app.get('/auth/profile', getProfile);  //this will get the profile of the user
+
+app.post('/auth/logout', logout);  //this will logout the user
 
 // Serve the front-end application from the `client` folder
 app.use(express.static('client'));
--- week3/prep-exercise/server/users.js
+++ week3/prep-exercise/server/users.js
@@ -1,12 +1,70 @@
-import newDatabase from './database.js'
+import newDatabase from './database.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-// Change this boolean to true if you wish to keep your
-// users between restart of your application
-const isPersistent = false
-const database = newDatabase({isPersistent})
+const isPersistent = true;
+const database = newDatabase({ isPersistent });
+const SECRET_KEY = '25b3a1a394281e1cd1ba35b5d145fe319057f77927347a4e0b5903a195df3a13';
 
-// Create middlewares required for routes defined in app.js
-// export const register = async (req, res) => {};
+export const register = async (req, res) => {
+  const { username, password } = req.body;
 
-// You can also create helper functions in this file to help you implement logic
-// inside middlewares
+  if(!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required!' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = database.create({ username, password: hashedPassword });
+
+  res.status(201).json({ id: user.id, username: user.username });
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if(!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required!' });
+  }
+  
+  const user = database.getByUsername(username);
+
+  if(!user) {
+    return res.status(400).json({ message: 'User not found!' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordValid) {
+    return res.status(400).json({ message: 'Invalid password!' });
+  }
+
+  const token = jwt.sign({ id: user.id }, SECRET_KEY);
+
+  res.status(201).json({ token });
+};
+
+export const getProfile = (req, res) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if(!token) {
+    return res.status(401).json({ message: 'Authorization token is required!' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const user = database.getById(decoded.id);
+
+    if(!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    res.status(200).json({ id: user.id, username: user.username });
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid or expired token.' });
+  }
+};
+
+export const logout = (req, res) => {
+  res.status(204).end();
+};
